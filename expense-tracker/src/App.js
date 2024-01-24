ac869d615ad5d76ee6f2313cff531b1cebe6c7f0
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2024-01-23 20:27:43 -0500

    bout to start the grid/cards

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,12 +1,16 @@
-import { Stack } from "react-bootstrap"
+import { Button, Stack } from "react-bootstrap"
 import Container from "react-bootstrap/Container"
 
 
 function App() {
   return (
-    <Container>
+    <Container className="my-4">
+      {/* This className give us margin spacing from the top for the header and buttons */}
       <Stack direction="horizontal" gap="2" className="mb-4">
+        {/* gives the header and buttons to line up horizontally at the top of the page */}
         <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary">Add Budget</Button>
+          <Button variant="outline-primary">Add Expense</Button>
       </Stack>
     </Container>
     
